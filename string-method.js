df0057b5688f7e1e0525260102706fdff947a703
Khishigdari <nbhishgee22@gmail.com>
2025-07-27 22:19:28 +0800

    string-method

---
+++ string-method.js
@@ -0,0 +1,7 @@
+let name = khishigdari;
+//function -> method
+console.log("name.length = ", name.length);
+console.log("name.charAt = ", name.charAt(0));
+console.log("name[1] = ", name[1]);
+console.log("name.substring(0, 2) = ", name.substring(0, 2));
+console.log('name.split("") = ', name.split(""));
