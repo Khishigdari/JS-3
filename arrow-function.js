5088e72cfaaa835ceef804b15b2b4dc74ffe0b22
Khishigdari <nbhishgee22@gmail.com>
2025-07-29 08:54:06 +0800

    arrow-function added

---
+++ arrow-function.js
@@ -0,0 +1,10 @@
+function sayHello() {
+    return "Hello World"
+}
+
+const sayHello1 = () => {
+    return "Hello World 1"
+}
+
+console.log(sayHello())
+console.log(sayHello1())
--- string-method.js
+++ string-method.js
@@ -1,7 +1,8 @@
-let name = "khishigdari";
+let name = "Khishigdari";
 //function -> method
 console.log("name.length = ", name.length);
 console.log("name.charAt = ", name.charAt(0));
 console.log("name[1] = ", name[1]);
 console.log("name.substring(0, 2) = ", name.substring(0, 2));
 console.log('name.split("") = ', name.split(""));
+console.log("name.toLowerCase() = ", name.toUpperCase())
